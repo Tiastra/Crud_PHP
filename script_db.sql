1a4ea236bd78a93d53e91e408fe8680460678314
Tiastra <tiagovza@hotmail.com>
2021-01-31 16:35:03 -0300

    Estrutura Laravel

---
+++ script_db.sql
@@ -0,0 +1,42 @@
+-- MySQL Workbench Synchronization
+-- Generated: 2021-01-31 11:19
+-- Model: New Model
+-- Version: 1.0
+-- Project: Name of the project
+-- Author: C.Tiago
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
+
+CREATE TABLE IF NOT EXISTS `mydb`.`Curso` (
+  `idCurso` INT(11) NOT NULL AUTO_INCREMENT,
+  `descricao_assunto` VARCHAR(100) NOT NULL,
+  `data_inicio` DATETIME NOT NULL,
+  `data_fim` DATETIME NOT NULL,
+  `quantidade_alunos` INT(11) NULL DEFAULT NULL,
+  `codigo_categoria` INT(11) NOT NULL,
+  PRIMARY KEY (`idCurso`),
+  INDEX `fk_Curso_Categoria_idx` (`codigo_categoria` ASC) VISIBLE,
+  CONSTRAINT `fk_Curso_Categoria`
+    FOREIGN KEY (`codigo_categoria`)
+    REFERENCES `mydb`.`Categoria` (`codigo`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE IF NOT EXISTS `mydb`.`Categoria` (
+  `codigo` INT(11) NOT NULL AUTO_INCREMENT,
+  `descricao` VARCHAR(30) NOT NULL,
+  PRIMARY KEY (`codigo`),
+  UNIQUE INDEX `descricao_UNIQUE` (`descricao` ASC) VISIBLE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
